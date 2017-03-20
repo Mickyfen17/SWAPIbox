3d56d4ecf9022abe185872d07a6d988c993694aa
Michael Fenwick <mickyfen17@aol.com>
2017-03-20 16:29:34 -0600

    App now recieving random film crawl, saving to state & render

--- src/container/App.js
+++ src/container/App.js
@@ -6,25 +6,32 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-
+      openingCrawl: {}
     }
   }
 
   componentDidMount() {
-    fetch('https:swapi.co/api/planets/')
+    fetch('https://swapi.co/api/films/')
     .then((response) => {
       return response.json()
     })
     .then((json) => {
-      console.log(json);
+      const index = Math.floor(Math.random() * json.count);
+      const { opening_crawl, title, release_date } = json.results[index];
+      this.setState({
+        openingCrawl : { opening_crawl, title, release_date }
+      })
     })
   }
 
   render() {
+    const { opening_crawl, title, release_date } = this.state.openingCrawl
     return (
       <div className="App">
         <h1>SWAPIbox</h1>
-
+        <p>{ opening_crawl }</p>
+        <h3>{ title }</h3>
+        <h4>{ release_date }</h4>
       </div>
     );
   }
