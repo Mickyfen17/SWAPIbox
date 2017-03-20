f7b6adf4ca7339e8c0c536d2301bda8a9ac8e01e
Kenia Farias <11431kenia@gmail.com>
2017-03-20 14:21:02 -0600

    add import carlist

--- src/container/App.js
+++ src/container/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardList from '../components/CardList/CardList'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>SWAPIbox</h1>
 
       </div>
     );
