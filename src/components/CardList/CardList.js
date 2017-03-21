21c5906296bc35e33866a2575800be68ee618cc3
Kenia Farias <11431kenia@gmail.com>
2017-03-21 13:27:30 -0600

    adds peoplecards nehiclecards and plantecards to render

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-const CardList = () => {
+const CardList = (props) => {
+  return (
+    <div>
+      <PeopleCards people={props.people}/>
+      <VehicleCards vehicles={props.vehicles}/>
+      <PlanetCards planets={props.planets}/>
+    </div>
+  )
 
 }
 
