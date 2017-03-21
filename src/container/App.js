f7857e43623a5f4500fd3d4cdcec186fde6f4de4
Kenia Farias <11431kenia@gmail.com>
2017-03-21 13:28:40 -0600

    add new people vehicle and planet components and render the in card list, the render carlist in main app

--- src/container/App.js
+++ src/container/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import OpeningHeader from '../components/OpeningHeader/OpeningHeader'
 import Buttons from '../components/Buttons/Buttons'
+import CardList from '../componets/CardList/CardList'
 
 class App extends Component {
   constructor() {
@@ -53,6 +54,7 @@ class App extends Component {
         <h1 className='main-header'>SWAPI-box</h1>
         <Buttons handleClick={ this.handleClick }/>
         <OpeningHeader { ...this.state.openingCrawl } />
+        <CardList people={this.state.people} vehicles={this.state.vehicles} planets={this.state.planets}/>
       </div>
     );
   }
