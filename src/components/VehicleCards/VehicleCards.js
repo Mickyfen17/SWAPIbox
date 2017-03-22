b1f749099b9476ba678029b802597b587357943f
Michael Fenwick <mickyfen17@aol.com>
2017-03-22 14:40:57 -0600

    cards now stateful and toggle fav on button click

--- src/components/VehicleCards/VehicleCards.js
+++ src/components/VehicleCards/VehicleCards.js
@@ -1,27 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './VehicleCards.css';
 
 
-const VehicleCards = ({ vehicles }) => {
-
-  const vehicleList = vehicles.map((vehicle, i) => {
-    const { name, model, vehicle_class, passengers } = vehicle
-    return(
-      <article className='display-card' key={i}>
+class VehicleCards extends Component {
+  constructor() {
+    super()
+    this.state = {
+      fav: false
+    }
+  }
+  render() {
+    const { name, model, vehicle_class, passengers } = this.props.eachVehicle
+    return (
+      <article className='display-card'>
         <h2 className='display-card-header' >{ name }
-          <button className='fav-button'></button>
+          <button
+            onClick={ () => this.setState({ fav: !this.state.fav }) }
+            className='fav-button'>
+          </button>
         </h2>
         <h4 className='display-card-text first-header' ><span>Model :</span> { model }</h4>
         <h4 className='display-card-text' ><span>Class :</span> { vehicle_class }</h4>
         <h4 className='display-card-text' ><span>Passengers :</span> { passengers }</h4>
       </article>
-    )
-  })
-  return (
-    <div className='vehicle-wrapper'>
-      { vehicleList }
-    </div>
-  )
+    );
+  }
 }
 
 export default VehicleCards;
