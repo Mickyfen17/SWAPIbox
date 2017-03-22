591b55ec91723453798f710259500471d7a2faa7
Michael Fenwick <mickyfen17@aol.com>
2017-03-21 22:45:09 -0600

    vehicle details now displaying to DOM

--- src/components/VehicleCards/VehicleCards.js
+++ src/components/VehicleCards/VehicleCards.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import './VehicleCards.css';
 
-const VehicleCards = (props) => {
+
+const VehicleCards = ({ vehicles }) => {
+
+  const vehicleList = vehicles.map((vehicle, i) => {
+    const { name, model, vehicle_class, passengers } = vehicle
+    return(
+      <article className='display-card' key={i}>
+        <h2 className='display-card-header' >{ name }
+          <button className='fav-button'></button>
+        </h2>
+        <h4 className='display-card-text' ><span>Model :</span> { model }</h4>
+        <h4 className='display-card-text' ><span>Class :</span> { vehicle_class }</h4>
+        <h4 className='display-card-text' ><span>Passengers :</span> { passengers }</h4>
+      </article>
+    )
+  })
   return (
     <div>
-      VehicleCards
+      { vehicleList }
     </div>
   )
 }
