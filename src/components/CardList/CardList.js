5c23186747edeb666e3d979b9f7dfb5e709cbab9
Michael Fenwick <mickyfen17@aol.com>
2017-03-22 14:33:12 -0600

    add vehcile map for vechicle cards

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -2,16 +2,24 @@ import React from 'react';
 import PeopleCards from '../PeopleCards/PeopleCards';
 import VehicleCards from '../VehicleCards/VehicleCards';
 import PlanetCards from '../PlanetCards/PlanetCards';
+import './CardList.css';
+
 
 const CardList = ({ people, vehicles, planets }) => {
+
+  const renderVehicle = vehicles.map((vehicle, i) => {
+    return (
+      <VehicleCards key={i} eachVehicle={ vehicle } />
+    )
+  })
+
   return (
-    <div>
+    <div className='card-wrapper' >
       { people.length !== 0 && <PeopleCards people={ people } /> }
-      { vehicles.length !== 0 && <VehicleCards vehicles={ vehicles } /> }
+      { vehicles.length !== 0 && renderVehicle }
       { planets.length !== 0 && <PlanetCards planets={ planets } /> }
     </div>
   )
-
 }
 
 export default CardList;
