3696562b1c9618a9845b8b7a5e8bf731b7bbe695
Kenia Farias <11431kenia@gmail.com>
2017-03-23 12:51:11 -0600

    added cardlist test

--- src/components/CardList/CardList.test.js
+++ src/components/CardList/CardList.test.js
@@ -4,8 +4,20 @@ import CardList from './CardList';
 
 describe('testing CardList', () => {
 
-  it('', () => {
-
-  })
-
-})
+  it('should render Person cards if passed People props ', () => {
+    const mockPeople = [
+   { name: 'planet',
+     species: 'kangaroos',
+     population: 100,
+     climate: 'icy',
+     language: 'piglatin',
+     homeworld: []
+   }];
+    const wrapper = mount( <CardList
+                              selectedContent={ mockPeople }
+                              category='people'
+                              />)
+    expect(wrapper.find('.card-wrapper').length).toBe(1);
+    // expect(wrapper.find('PeopleCards').length).toBe(1);
+  });
+});
