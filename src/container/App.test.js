a551ddb772a0a8aa8b18f507edb95601f875884e
Michael Fenwick <mickyfen17@aol.com>
2017-03-23 09:45:39 -0600

    add further tests to app

--- src/container/App.test.js
+++ src/container/App.test.js
@@ -21,9 +21,75 @@ describe('testing App', () => {
     const state = wrapper.state();
 
     expect(state.openingCrawl).toEqual({})
-    expect(state.people).toEqual([])
-    expect(state.vehicles).toEqual([])
-    expect(state.planets).toEqual([])
+    expect(state.selectedContent).toEqual([])
+    expect(state.catergory).toEqual('')
+    expect(state.favorites).toEqual([])
+    expect(state.toggleFavs).toEqual(false)
+  })
+
+  it('should contain a favorite button', () => {
+    const wrapper = shallow( <App /> )
+
+    expect(wrapper.find('.favorite-btn')).toHaveLength(1)
+  })
+
+  it('should be able to click on favorites button and toggle state', () => {
+    const wrapper = mount( <App /> )
+    const favBtn = wrapper.find('.favorite-btn');
+    let state = wrapper.state();
+
+    expect(state.toggleFavs).toEqual(false);
+
+    favBtn.simulate('click')
+    state = wrapper.state()
+
+    expect(state.toggleFavs).toEqual(true);
+  })
+
+  it('should display warning when favorites button is click and no favorite are found', () => {
+    const wrapper = mount( <App /> )
+    const favBtn = wrapper.find('.favorite-btn');
+
+    expect(wrapper.find('.no-fav-header')).toHaveLength(0);
+    favBtn.simulate('click')
+    expect(wrapper.find('.no-fav-header')).toHaveLength(1);
+  })
+
+  it('should display a favorite card button is click and a favorite prop is passed', () => {
+    const mockFavorite = {
+      cargo_capacity: '1234 lbs',
+      name: 'Skycrawler',
+      model: 'abc',
+      vehicle_class: 'Aircraft',
+      passengers: '25',
+      handleFavorites: () => {}
+    }
+    const wrapper = mount( <App /> )
+    const favBtn = wrapper.find('.favorite-btn');
+
+    wrapper.setState({ favorites: [ mockFavorite ]})
+    favBtn.simulate('click')
+
+    expect(wrapper.find('.display-card').text())
+    .toEqual('SkycrawlerModel : abcClass : AircraftPassengers : 25');
+  })
+
+  it('should display movie crawl content on origial render', () => {
+    const mockCrawl = {
+      opening_crawl: 'Luke Skywalker has vanished!',
+      release_date: '2015-12-11',
+      title: 'The Force Awakens'
+    }
+    const wrapper = mount( <App /> )
+    const state = wrapper.state();
+
+    expect(wrapper.find('.opening-header').text())
+    .toEqual('');
+
+    wrapper.setState({ openingCrawl: mockCrawl})
+
+    expect(wrapper.find('.opening-header').text())
+    .toEqual('Luke Skywalker has vanished!The Force Awakens2015-12-11');
   })
 
 })
